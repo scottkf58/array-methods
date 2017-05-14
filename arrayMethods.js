3ceaf70bb1d0c31d18c62a2bfb604b67dede1824
Scott Fujimoto <scottfujimoto@Scotts-MacBook-Pro.local>
2017-05-13 19:47:14 -1000

    another one

--- arrayMethods.js
+++ arrayMethods.js
@@ -22,3 +22,14 @@ nameArray.push("Scott", "Fujimoto");
 genericNumberArray.push(6, 7, 8, 9, 10);
 
 var popResult = colors.pop();
+
+scrambledWords.reverse();
+
+phoneNumber.reverse();
+
+var nextOrder = orderQueue.shift();
+
+mixedNums.sort();
+
+mixedWords.sort();
+
